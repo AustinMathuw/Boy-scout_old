6dd6d431b209aa62c29fb8775649191bb8ea2e05
AustinMathuw <siraustin1234@gmail.com>
2015-10-23 10:33:08 -0400

    Update MainJavaScript.js

--- Source/mobile/MainJavaScript.js
+++ Source/mobile/MainJavaScript.js
@@ -3,22 +3,22 @@
     $('.menu_text').click(function () {
         $('.menu').animate({
             left: "0px"
-        }, 4);
+        }, 200);
 
         $('body').animate({
             left: "600px"
-        }, 4);
+        }, 200);
     });
 
     /* Then push them back */
     $('.menu-close').click(function () {
         $('.menu').animate({
             left: "-600px"
-        }, 4);
+        }, 200);
 
         $('body').animate({
             left: "0px"
-        }, 4);
+        }, 200);
     });
 };
 
